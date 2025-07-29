08fb26abae51bb1a87736d963fdce802df711cc1
Matheus Pimentel <149837292+matheuspimentel1@users.noreply.github.com>
2025-07-29 05:25:19 -0300

    Update process.php

--- process.php
+++ process.php
@@ -1,14 +1,16 @@
 <?php ob_start();
-	if(isset($_POST['submit'])) {
-	$to = "pimentel.matheus00@gmail.com";
-	$subject = $_POST['subject'];
-	$email = $_POST['email'];
-	$message = $_POST['message'];
-	// Always set content-type when sending HTML email
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-	$headers .= "From: " .$email . "\r\n";
-	mail($to,$subject,$message,$headers);
-	header("Location: contact.html");
-	}
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+ if(isset($_POST['submit'])) {
+  $to = "pimentel.matheus00@gmail.com";
+  $subject = htmlspecialchars($_POST['subject']);
+  $email = htmlspecialchars($_POST['email']);
+  $message = htmlspecialchars($_POST['message']);
+  // Always set content-type when sending HTML email
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+  $headers .= "From: " .$email . "\r\n";
+  mail($to,$subject,$message,$headers);
+  header("Location: contact.html");
+ }
+}
 ?>
